1302b7de2d36922d6d82540988af6591b4064905
Martin Marmolejo <martinalejandro.marmolejo@gmail.com>
2026-08-19 16:37:04 -0700

    Trabajo con estructuras de control

---
+++ Curso/estructuras_de_control.py
@@ -0,0 +1,43 @@
+#####Estruncturas Condicionales#####
+
+edad = 61
+
+# IF
+# if edad >= 18:
+#     print("Mayor de edad")
+
+# # IF ELSE
+# if edad >= 18:
+#     print("Mayor de edad")
+# else:
+#     print("Menor de edad")
+
+# #IF ELIF ELSE
+calificacion = 85
+
+if calificacion >= 90:
+   print ("Excelente")
+
+elif calificacion >= 80:
+   print ("Muy bueno")
+
+elif calificacion >= 70:
+   print ("Bueno")
+
+else:
+   print ("Necesita mejorar")
+
+
+# EJERCICIO
+
+if edad < 18:
+    print("Eres menor de edad")
+
+elif edad == 60:
+    print("¡Feliz 60 cumpleaños!")
+
+elif edad > 60:
+    print("Eres adulto mayor")
+
+else:
+    print("Eres un adulto")
