e452bd3c24b657f4e03cc0a804893517a4af0a75
Martin Marmolejo <martinalejandro.marmolejo@gmail.com>
2026-08-19 15:51:51 -0700

    Tipo de datos Python

---
+++ Curso/datos.py
@@ -0,0 +1,17 @@
+# Tipo de datos
+
+#int
+edad = 25
+cantidad = 100
+
+#float
+precio = 9.99
+altura = 1.75
+
+#string
+nombre = "Martin"
+mensaje = '¡Hola, Mundo!'
+
+#bool
+es_mayor_de_edad = True
+tiene_descuento = False
